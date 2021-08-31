d16e4d9e43860b4dcad2bcd73e3e1c3d35fbdf87
Roberto Morales <roberto@morales.com.co>
2021-08-30 19:19:46 -0500

    Variables y operaciones en python

--- clase_uno.py
+++ clase_uno.py
@@ -6,3 +6,86 @@ Created on Mon Aug 30 18:50:09 2021
 @author: robertomorales
 """
 
+print('Hello world')
+
+# Variables
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+# Operaciones
+
+# Suma
+
+a = 5
+b = 2
+c = a + b
+print(c)
+
+# Resta
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicación
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# División
+
+a = 5
+b = 2
+c = a / b
+print(c)
+
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# Raiz
+
+a = 27
+c = a ** (1/3)
+print(c)
+
+# import math
+# raiz = math.sqrt(25)
+# print(raiz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
