72e5f35a3102d19842f11770abb7251027c5c479
Roberto Morales <roberto@morales.com.co>
2021-09-06 19:25:32 -0500

    Tabla de verdad, Jerarquía de operaciones y condiciones

--- clase_dos.py
+++ clase_dos.py
@@ -5,4 +5,53 @@ Created on Mon Sep  6 19:02:43 2021
 
 @author: robertomorales
 """
+# Condicionales
+
+# Tabla de Verdad
+
+# Tabla del and
+# v and v = v
+# v and f = f
+# f and v = f
+# f and f = f
+
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# Tabla del or
+# v or v = v
+# v or f = v
+# f or v = v
+# f or f = f
+
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+# Negación
+
+print(not True)  # False
+print(not False)  # True
+
+# Mas de dos condiciones al mismo tiempo
+
+print(True and False and True or False or True or  True) # True
+print(True and (False and True) or False or (True or True)) #True
+
+# Jerarquía de operaciones
+# 1. Paréntesis y llaves
+# 2. Potencias y Raices
+# 3. Multiplicación y División
+# 4. Sumas y Restas
+
+# Jerarquía de operaciones booleanas
+# 1. Paréntesis y llaves
+# 2. Tabla de verdad
+
+
+
+
 
